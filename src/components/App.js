3d1f64e8e1016568d99077dac42e5371e26e5992
Tyler Gregg <tgregg@etcgrp.com>
2019-04-20 00:22:38 -0600

    init commit

---
+++ src/components/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { simpleAction } from '../redux/simpleAction'
+
+/* 
+ * mapDispatchToProps
+*/
+const mapDispatchToProps = dispatch => ({
+  simpleAction: () => dispatch(simpleAction())
+})
+
+/* 
+ * mapStateToProps
+*/
+const mapStateToProps = state => ({
+  ...state
+})
+
+
+class App extends Component {
+
+  simpleAction = (event) => {
+    this.props.simpleAction();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <pre>
+          {
+            JSON.stringify(this.props)
+          }
+        </pre>
+        <button onClick={this.simpleAction}>Test redux action</button>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
